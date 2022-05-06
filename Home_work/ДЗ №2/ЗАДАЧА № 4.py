5e0e93a6e6e4d8eced4088bd9cefcc650f684687
Markinmarch <iztvmrk@mail.ru>
2022-05-06 18:54:10 +0300

    main

--- Home_work/ДЗ №2/ЗАДАЧА № 4.py
+++ Home_work/ДЗ №2/ЗАДАЧА № 4.py
@@ -16,14 +16,20 @@ while True:
     if kids >= 3:
         bet_kids = base_bet - 1
         print('Ваша ипотечная ставка снижена на 1%')
+    else:
+        bet_kids = base_bet
     check_client = input('Имеется ли у Вас в нашем банке зарплатный проект (да/нет): ')
     if check_client == 'да':
         bet_check_client = bet_kids - 0.5
         print(f'Спасибо, {name}, что пользуетесь нашими услугами! Ваша ставка снижена на 0.5%!')
-    check_insurance = input('Оформлена ли у Вас в нашем банке страховка (да/нет): ')
+    else:
+        bet_check_client = bet_kids
+    check_insurance = str(input('Оформлена ли у Вас в нашем банке страховка (да/нет): '))
     if check_insurance == 'да':
         bet_check_insurance = bet_check_client - 1.5
         print(f'Спасибо, {name}, что пользуетесь нашими услугами! Ваша ставка снижена на 1.5%!')
+    else:
+        bet_check_insurance = bet_check_client
     print(f'Спасибо, {name}, что воспользовались нашим банком для оформления ипотеки!')
     print('')
     print(f'Ваша ипотечная ставка в регионе: {region} составляет {bet_check_insurance}%')
