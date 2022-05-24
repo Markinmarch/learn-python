539798740340e868f359faedbd8f744ee07a4ef7
Markinmarch <iztvmrk@mail.ru>
2022-05-24 17:49:12 +0300

    main

---
+++ Home_work/ДЗ №5/README.md
@@ -0,0 +1,6 @@
+**Задача № 1**
+
+
+**ДЗ**
+https://github.com/netology-code/py-homeworks-basic/tree/master/5.functions
+
---
+++ Home_work/ДЗ №5/Задача № 1.py
@@ -0,0 +1,20 @@
+documents = [
+        {"type": "passport", "number": "2207 876234", "name": "Василий Гупкин"},
+        {"type": "invoice", "number": "11-2", "name": "Геннадий Покемонов"},
+        {"type": "insurance", "number": "10006", "name": "Аристарх Павлов"}
+      ]
+directories = {
+        '1': ['2207 876234', '11-2', '5455 028765'],
+        '2': ['10006'],
+        '3': []
+      }
+def people():
+    num_doc = input('Введите номер документа: ')
+
+# def shelf():
+#     num_doc = input('Введите номер документа: ')
+#     for doc_num in directories:
+#         if doc_num == True:
+def list_():
+    name_man = input('Введите имя и фамилию: ')
+ 
