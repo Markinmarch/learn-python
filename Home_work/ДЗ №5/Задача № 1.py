3f859ae995805514288dd7325b121f5aa8021a44
Markinmarch <iztvmrk@mail.ru>
2022-05-26 15:44:43 +0300

    main

--- Home_work/ДЗ №5/Задача № 1.py
+++ Home_work/ДЗ №5/Задача № 1.py
@@ -1,20 +1,25 @@
 documents = [
-        {"type": "passport", "number": "2207 876234", "name": "Василий Гупкин"},
-        {"type": "invoice", "number": "11-2", "name": "Геннадий Покемонов"},
-        {"type": "insurance", "number": "10006", "name": "Аристарх Павлов"}
+        {"type": "passport", "number": "2207 876234", "name": "Василий Питонов"},
+        {"type": "invoice", "number": "11-2", "name": "Иерофан Покемонов"},
+        {"type": "insurance", "number": "10006", "name": "Эдуард Шевелюров"}
       ]
 directories = {
         '1': ['2207 876234', '11-2', '5455 028765'],
         '2': ['10006'],
         '3': []
       }
-def people():
-    num_doc = input('Введите номер документа: ')
 
-# def shelf():
-#     num_doc = input('Введите номер документа: ')
-#     for doc_num in directories:
-#         if doc_num == True:
-def list_():
-    name_man = input('Введите имя и фамилию: ')
- 
+def get_people_name(name):
+  what_number_doc = input('Введите номер документа: ')
+  for people in name:
+    if what_number_doc == people['number']:
+      return(people['name'])
+# print(get_people_name(documents))
+
+def get_shelf_number(shelf_number):
+  what_number_doc = input('Введите номер документа: ')
+  for shelf_num in shelf_number:
+    return shelf_number.keys()
+print(get_shelf_number(directories))
+
+
