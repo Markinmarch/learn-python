0d78fe166af84463aaa014a183cfd4555350ac5e
Markinmarch <iztvmrk@mail.ru>
2022-05-26 19:40:48 +0300

    main

--- Home_work/ДЗ №5/Задача № 1.py
+++ Home_work/ДЗ №5/Задача № 1.py
@@ -18,8 +18,40 @@ def get_people_name(name):
 
 def get_shelf_number(shelf_number):
   what_number_doc = input('Введите номер документа: ')
-  for shelf_num in shelf_number:
-    return shelf_number.keys()
-print(get_shelf_number(directories))
+  for shelf_value in shelf_number:
+    if what_number_doc in shelf_number.get(shelf_value):
+      return(f'Документ находится на полке № {shelf_value[0]}')
+  return('Отсутствуют документы с данным номером!')
+# print(get_shelf_number(directories))
 
+def get_list(docs):
+  for doc in docs:
+    return (f"{doc['type']} {doc['number']} {doc['name']}")
+print(get_list(documents))
 
+def add_doc(docs_add):
+  shelf = input('Введите номер полки куда положить документ. ')
+  if shelf not in docs_add:
+      return 'Нет такой полки'
+  doc = {}
+  for info in ('type', 'number', 'name'):
+      doc[info] = input(f'{info}: ')
+  docs_add[shelf] = docs_add.get(shelf).append(doc['number'])
+  return 'Документ добавлен'
+
+# while True:
+#   print('Возможные команды: p, s, l, a')
+#   comand = input('Введите название команды ')
+ 
+#   if comand == 'p':
+#     print(get_people_name(documents))
+    
+#   elif comand == 's':
+#     print(get_shelf_number(directories))
+    
+#   elif comand == 'l':
+#     print(get_list(documents))
+    
+#   elif comand == 'a':
+#     print(add_doc(directories))
+# print(documents[::])
