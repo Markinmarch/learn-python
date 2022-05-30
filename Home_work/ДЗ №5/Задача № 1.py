f8c9e4e74d3b2b71653abfd6c9ab4ce0d8fca3bc
Markinmarch <iztvmrk@mail.ru>
2022-05-30 14:41:03 +0300

    main

--- Home_work/ДЗ №5/Задача № 1.py
+++ Home_work/ДЗ №5/Задача № 1.py
@@ -14,6 +14,7 @@ def get_people_name(name):
   for people in name:
     if what_number_doc == people['number']:
       return(people['name'])
+  return('Номера не существует!')
 # print(get_people_name(documents))
 
 def get_shelf_number(shelf_number):
