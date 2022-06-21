62522cf9a76b4d670dcbf1c3e96afcc21a85a905
Mark Izotov <iztvmrk@mail.ru>
2022-06-21 16:43:20 +0300

    main

--- Home_work/ДЗ №7/Задача № 1.py
+++ Home_work/ДЗ №7/Задача № 1.py
@@ -1,6 +1,6 @@
 # открытие файла и чтение:
 def open_cook_book():
     with open('ДЗ №7\dish_list.txt', 'r', encoding='utf-8') as file:
-        dish_dict = {file.read()}
-        print(dish_dict)
+        cook_book = {file.read()}
+        print(cook_book)
 open_cook_book()
