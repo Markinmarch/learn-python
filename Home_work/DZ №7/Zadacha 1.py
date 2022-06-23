b55815419868645b688c2bebe277d72299c52127
Mark Izotov <iztvmrk@mail.ru>
2022-06-23 07:52:31 +0300

    main

--- Home_work/DZ №7/Zadacha 1.py
+++ Home_work/DZ №7/Zadacha 1.py
@@ -4,7 +4,11 @@ from ctypes import Structure
 def open_file(file_name):
     with open(file_name, 'r', encoding='utf-8') as file:
         return file.read()
-# file_show = open_file('DZ №7\dish_list.txt')
-# print(file_show)
+file_show = open_file('DZ №7\dish_list.txt').strip()
+print(file_show)
 
-# def split_file(split_text):
+def split_file(split_text):
+    big_dish_list = split_text.split('\n')
+    print(big_dish_list)
+
+q = split_file(file_show)
