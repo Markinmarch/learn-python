5c6ad7fb2b0a68f5c081ce73f2d15cab4cbee94d
Mark Izotov <iztvmrk@mail.ru>
2022-06-27 22:43:55 +0300

    main

--- Home_work/DZ №7/Zadacha 1.py
+++ Home_work/DZ №7/Zadacha 1.py
@@ -3,14 +3,20 @@ def open_file(file_name):
     with open(file_name, 'r', encoding='utf-8') as file:
         return file.read().strip().split('\n\n')
         
-file_show = open_file('DZ №7\dish_list.txt')
+file_show = open_file('Home_work\DZ №7\dish_list.txt')
 
 # структурируем данные в требуемый вид с учётом задания
-def structure_data(dish_list):
-    for item in dish_list:
-        res = item.split('\n')
-        for i in res[2:]:
-            ras = i.split(' | ')
-            cook_book = {res[0]:[{'ingredient_name': ras[0]}, {'quantity': ras[1]}, {'measure': ras[2]}]}
-            print(cook_book)
+def structure_data(file_name):
+    r = file_name[0].split('\n')
+    # print(r[2:])
+    for i in r[2:]:
+        l = []
+        x = i.split(' | ')
+        l.append(i)
+        print(x)
+
+
+
+    
+
 structure_data(file_show)
