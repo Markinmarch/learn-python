1a34d4dd4b5b2b2f6775f238187f6267fb53392c
Mark Izotov <iztvmrk@mail.ru>
2022-07-06 07:01:06 +0300

    main

--- Home_work/DZ №7/Zadacha 3.py
+++ Home_work/DZ №7/Zadacha 3.py
@@ -1,11 +1,23 @@
-# открываем файл и читаем
-def read_file(file_path):
+catal_ad = 'Home_work/DZ №7/text/'
+
+def mod_os(adress):
+    import os
+    file_list = os.listdir(adress)
+    return file_list
+
+file_list = mod_os(catal_ad)
+
+def open_file(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
         return file.read()
 
-# открываем файл и читаем
-def write_file(file_path):
-    with open(file_path, 'r', encoding='utf-8') as file:
-        return file.write()
 
-file = read_file('Home_work\DZ №7\text\1.txt')
+for i in range(0, len(file_list)):
+    read = open_file(catal_ad + file_list[i])
+    
+
+
+def how_lines(file):
+    return len(file.strip().split('\n'))
+
+# print(how_lines(read))
