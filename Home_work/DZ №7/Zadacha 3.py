cf7dd635fb9c36f97b6f0969d7f0a7ea79f2dd05
Mark Izotov <iztvmrk@mail.ru>
2022-07-06 23:23:25 +0300

    main

--- Home_work/DZ №7/Zadacha 3.py
+++ Home_work/DZ №7/Zadacha 3.py
@@ -1,6 +1,3 @@
-from fileinput import filename
-
-
 catal_ad = 'Home_work/DZ №7/text/'
 
 def mod_os(adress):
@@ -10,20 +7,32 @@ def mod_os(adress):
 
 file_list = mod_os(catal_ad)
 
-def sluzhebka(lst, adress):
-    return lst, adress
-print(sluzhebka(file_list, catal_ad))
-
 def open_file(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
         return file.read().strip()
+
+def sluzhebka(lst, adress):
+    where_is_the_file_from = [(adress + lst[i]) for i in range(0, len(lst))]
+    return where_is_the_file_from
+ready_to_adress_file = sluzhebka(file_list, catal_ad)
+print(ready_to_adress_file)
+
+def how_many_lines(lst, adress):
+    num_list_line = []
+    how_many_lines = [open_file(adress + lst[i]) for i in range(0, len(lst))]
+    for item in how_many_lines:
+        item = len(item.split('\n'))
+        num_list_line.append(item)
+    return num_list_line
+ready_to_lines = how_many_lines(file_list, catal_ad)
+print(ready_to_lines)
 
 def sort_ready_text(lst, adress):
     read = [open_file(adress + lst[i]) for i in range(0, len(lst))]
     ready_text = sorted(read, key = len)
     return '\n'.join(ready_text)
 read = sort_ready_text(file_list, catal_ad)
-# print(read)
+print(read)
 
 def write_file(ready_file):
     with open(r'Home_work/DZ №7/new_file.txt', 'w', encoding='utf-8') as file:
@@ -31,4 +40,3 @@ def write_file(ready_file):
 
 # write_file(read)
 
-# print(how_lines(read))
