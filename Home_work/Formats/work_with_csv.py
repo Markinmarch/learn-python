7e8ade3152ea42bafc3e1f0f0ebfdeb60bf7a9aa
Mark Izotov <iztvmrk@mail.ru>
2022-07-13 18:35:37 +0300

    фикс

---
+++ Home_work/Formats/work_with_csv.py
@@ -0,0 +1,10 @@
+import csv
+from operator import delitem
+
+
+def open_file(file_name):
+    with open(file_name, 'r', encoding='1252') as file:
+        ready_to_read = csv.reader(file, delimiter=',')
+        return ready_to_read
+adress = 'C:/Users/User/Desktop/Study/netology_learning/Home_work/glff.csv'
+open_file(adress)
