5ca495ff6cbcce36e0a3655fdbcf39f409c93a9c
Mark Izotov <iztvmrk@mail.ru>
2022-07-21 23:44:37 +0300

    main

--- Home_work/DZ №8/Zadacha 1.py
+++ Home_work/DZ №8/Zadacha 1.py
@@ -1,9 +1,21 @@
-hero_nick = ['Hulk', 'Capitan America', 'Thanos']
-hero_skill = 'intelligence'
+import requests
 
-def who_is_smart(nick, skill):
-    import requests
-    url = 'https://akabab.github.io/superhero-api/api'
+hero_nick = ['Hulk', 'Captain America', 'Thanos']
+hero_skill = 'intelligence'
+url_adress = 'https://akabab.github.io/superhero-api/api/all.json'
+
+# Делаем запрос по адресу
+def makea_request(url):
     take_info = requests.get(url).json()
-    print(take_info)
-# who_is_smart(hero_nick, hero_skill)
+    return take_info
+ready_info = makea_request(url_adress)
+
+# Вытаскиваем данные по именам и выделяем с максимальным скиллом
+def extracting_a_value_by_name(nickname, skill):
+    for hero in ready_info:
+        for one_nick in nickname:
+            if hero.get('name') == one_nick:
+                for k, v in hero['powerstats'].items():
+                    if k == skill:
+                        print(hero['name'], ':', k, '=', v)
+ready_hero = extracting_a_value_by_name(hero_nick, hero_skill)
