04b87de533374529d8af3e74fdb634cbaf3d5998
Mark Izotov <iztvmrk@mail.ru>
2022-09-24 14:10:37 +0300

    main

--- Home_work/DZ_13/main.py
+++ Home_work/DZ_13/main.py
@@ -1,28 +1,37 @@
 import modules
 
-# создаём таблицу
+# функция создания таблицы БД
 modules.get_create_table()
 
-# добавляем клиента
+# функция добавляет нового пользователя             
 modules.add_user()
 
 # добавляем ещё один(!) телефон
 modules.add_phone()
-
+    
 # изменяем параметр name
 modules.edit_name()
 
 # изменяем параметр last_name
 modules.edit_last_name()
-
+    
 # изменяем параметр num_phone
 modules.edit_phone()
-
-# изменяем параметр num_phone
+    
+# изменяем параметр email
 modules.edit_email()
-
+    
+# удаление телефона пользователя            
+modules.delete_phone()
+    
 # удаление пользователя            
 modules.delete_user()
-
+    
 # поиск пользователя
 modules.search_data()
+
+# закрываем
+modules.conn.close()
+            
+                        
+            
