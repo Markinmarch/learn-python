d9667dfbf5d15ce8aa9a4221e9163884c0eb8964
Mark Izotov <iztvmrk@mail.ru>
2022-10-12 20:47:34 +0300

    main

--- Home_work/DZ_19_decorator/main.py
+++ Home_work/DZ_19_decorator/main.py
@@ -1,23 +1,29 @@
 import csv
+import datetime
+from re import S
 
-def log_info(enter_func):
+# def log_info(enter_func):
     
-    def func_enter(*args, **kwargs):
+#     with open(r'DZ_19_decorator/logs/csv_log.csv', 'w', encoding = 'utf-8') as file:
+#         writer = csv.writer(file)
+#         writer.writerows(
+#             [['Дата', 'Время', 'Имя функции', 'Аргументы']]
+#         )
         
-        enter_func(*args, **kwargs)
-        return enter_func
+#     def func_enter(*args, **kwargs):
+        
+#         enter_func(*args, **kwargs)
 
-    with open(r'DZ_19_decorator/logs/csv_log.csv', 'w', encoding = 'utf-8') as file:
-        writer = csv.writer(file)
-        writer.writerows(
-            [['Дата', 'Время', 'Имя функции', 'Аргументы']]
-        )
+    
         
-    # for item_pars in ready_pars_list():
-    #     elements_for_table = item_pars.split(' - ')
-    #     with open(r'DZ_17_web_scraping/work_files/file.csv', 'a', encoding = 'utf-8') as file:
-    #         writer = csv.writer(file)
-    #         writer.writerow(
-    #             [elements_for_table[0], elements_for_table[1], elements_for_table[2]]
-    #         )    
-    return enter_func
+#     # for item_pars in ready_pars_list():
+#     #     elements_for_table = item_pars.split(' - ')
+#     #     with open(r'DZ_17_web_scraping/work_files/file.csv', 'a', encoding = 'utf-8') as file:
+#     #         writer = csv.writer(file)
+#     #         writer.writerow(
+#     #             [elements_for_table[0], elements_for_table[1], elements_for_table[2]]
+#     #         )    
+st = 0
+
+date_today = datetime.datetime.now().strftime('%d-%m-%Y')
+print(date_today)
