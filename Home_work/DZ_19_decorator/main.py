ec8d8dc47fa2f1353fa96e48d6254a309abe5ac3
Mark Izotov <iztvmrk@mail.ru>
2022-10-15 02:41:46 +0300

    main

--- Home_work/DZ_19_decorator/main.py
+++ Home_work/DZ_19_decorator/main.py
@@ -5,6 +5,5 @@ if __name__ == '__main__':
     url = input('Введите адрес сайта: ')
     scrap_web.web_scrap(url)
     print('DZ_19_decorator/logs/csv_log.csv')
-# https://www.ozon.ru/
-# print(dir(web_scrap))
-# print(inspect.signature(web_scrap))
+
+#pigar
