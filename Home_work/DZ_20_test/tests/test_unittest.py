35a9ddd4e9d1a4dc83095b14c30b20c36eebb578
Mark Izotov <iztvmrk@mail.ru>
2022-10-17 20:27:01 +0300

    main

--- Home_work/DZ_20_test/tests/test_unittest.py
+++ Home_work/DZ_20_test/tests/test_unittest.py
@@ -1,4 +1,19 @@
 import unittest
+from unittest import result
 
 from DZ_5_dict_def import Zadacha_1
 
+class TestFunc(unittest.TestCase):
+    
+    def setUp(self) -> None:
+        print('setUp ==>')
+        
+    def tearDown(self) -> None:
+        print('tearDown')
+        
+    def test_Zadacha_1(self):
+        
+        result = 'Возможные команды: p, s, l, a'
+        etalon = 'Возможные команды: p, s, l, a'
+        self.assertEqual(etalon, result)
+        
