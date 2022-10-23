46868025cab0b7f7cf5d7645f6e564459e75457e
Mark Izotov <iztvmrk@mail.ru>
2022-10-23 22:07:54 +0300

    main

--- Home_work/DZ_20_test/tests/work_/buhgalteria_class.py
+++ Home_work/DZ_20_test/tests/work_/buhgalteria_class.py
@@ -1,10 +1,10 @@
 from data import buh_data
-
-documents = buh_data.documents
-directories = buh_data.directories
+    
+def get_input(text):
+    return input(text)
 
 class Buhgalteria():
-    
+    '''основной метод, который определяет переменные и функции'''
     def __init__(self,
                  name,
                  shelf_number,
@@ -16,19 +16,16 @@ class Buhgalteria():
         self.docs = docs
         self.docs_add = docs_add
         self.direct_add = direct_add
-    
-    def get_input(self, text):
-        return input(text)
-    
+
     def get_people_name(self):
-        what_number_doc = self.get_input('Введите номер документа: ')
+        what_number_doc = get_input('Введите номер документа: ')
         for people in self.name:
-            if what_number_doc == people['number']:
+            if what_number_doc in people['number']:
                 return(people['name'])
-            return('Номера не существует!')
+        return('Номера не существует!')
     
     def get_shelf_number(self):
-        what_number_doc = self.get_input('Введите номер документа: ')
+        what_number_doc = get_input('Введите номер документа: ')
         for shelf_value in self.shelf_number:
             if what_number_doc in self.shelf_number.get(shelf_value):
                 return(f'Документ находится на полке № {shelf_value[0]}')
@@ -39,19 +36,24 @@ class Buhgalteria():
             return f"{doc['type']} {doc['number']} {doc['name']}"
         
     def add_doc(self):
-        num_shelf = self.get_input('Введите номер полки, куда положить документ: ')
+        num_shelf = get_input('Введите номер полки, куда положить документ: ')
         if num_shelf not in self.direct_add.keys():
             return 'Такой полки не существует!'
         new_data = {}
         for Ddata in ('type', 'number', 'name'):
-            new_data[Ddata] = self.get_input(f'Введите данные: {Ddata}')
+            new_data[Ddata] = get_input(f'Введите данные: {Ddata}')
         self.docs_add.append(new_data)
         self.direct_add[num_shelf].append(new_data['number'])
         for doc in self.docs_add:
             print(doc['type'], doc['number'], doc['name'])
         return self.direct_add
-
-get_args_to_class = Buhgalteria(
+    
+class work_buh():
+    '''рабочий метод, в котором вводится команда'''
+    documents = buh_data.documents
+    directories = buh_data.directories
+    
+    get_args_to_class = Buhgalteria(
     documents,
     directories,
     documents,
@@ -59,19 +61,16 @@ get_args_to_class = Buhgalteria(
     directories
     )
 
-while True:
-    print('Возможные команды: p, s, l, a')
-    comand = input('Введите название команды ')
- 
-    if comand == 'p':
-        print(Buhgalteria.get_people_name(get_args_to_class))
+    while True:
+        print('Возможные команды: p, s, l, a')
+        comand = get_input('Введите название команды ')
+        if comand == 'p':
+            print(Buhgalteria.get_people_name(get_args_to_class))
+        elif comand == 's':
+            print(Buhgalteria.get_shelf_number(get_args_to_class))
+        elif comand == 'l':
+            print(Buhgalteria.get_list(get_args_to_class))
+        elif comand == 'a':
+            print(Buhgalteria.add_doc(get_args_to_class))
+        break
     
-    elif comand == 's':
-        print(Buhgalteria.get_shelf_number(get_args_to_class))
-    
-    elif comand == 'l':
-        print(Buhgalteria.get_list(get_args_to_class))
-    
-    elif comand == 'a':
-        print(Buhgalteria.add_doc(get_args_to_class))
-    break
