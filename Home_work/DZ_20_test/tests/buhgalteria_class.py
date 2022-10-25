2d1d1f4390a0aad17c7cac84d5366a0a4234b592
Mark Izotov <iztvmrk@mail.ru>
2022-10-26 01:00:51 +0300

    main

--- Home_work/DZ_20_test/tests/buhgalteria_class.py
+++ Home_work/DZ_20_test/tests/buhgalteria_class.py
@@ -1,58 +1,63 @@
 from data import buh_data
-    
-def get_input(text):
-    return input(text)
 
 class Buhgalteria():
     '''основной метод, который определяет переменные и функции'''
     def __init__(self,
-                 name,
-                 shelf_number,
-                 docs,
-                 docs_add,
-                 direct_add):
+                 name = buh_data.documents,
+                 shelf_number = buh_data.directories,
+                 docs = buh_data.documents,
+                 docs_add = buh_data.documents,
+                 direct_add = buh_data.directories
+                 ):
         self.name = name
         self.shelf_number = shelf_number
         self.docs = docs
         self.docs_add = docs_add
         self.direct_add = direct_add
 
-    def get_people_name(self):
-        what_number_doc = get_input('Введите номер документа: ')
+    def get_people_name(self, what_number_doc):
+        # what_number_doc = input('Введите номер документа: ')
         for people in self.name:
             if what_number_doc in people['number']:
                 return(people['name'])
         return('Номера не существует!')
     
-    def get_shelf_number(self):
-        what_number_doc = get_input('Введите номер документа: ')
+    def get_shelf_number(self, what_number_doc):
+        # what_number_doc = input('Введите номер документа: ')
         for shelf_value in self.shelf_number:
             if what_number_doc in self.shelf_number.get(shelf_value):
                 return(f'Документ находится на полке № {shelf_value[0]}')
-            return('Отсутствуют документы с данным номером!')
+        return('Отсутствуют документы с данным номером!')
         
     def get_list(self):
+        lst = []
         for doc in self.docs:
-            return f"{doc['type']} {doc['number']} {doc['name']}"
+            ien = f"{doc['type']} {doc['number']} {doc['name']}"
+            lst.append(ien)
+        return lst
         
-    def add_doc(self):
-        num_shelf = get_input('Введите номер полки, куда положить документ: ')
+    def add_doc(self, num_shelf, new_data):
+        # num_shelf = input('Введите номер полки, куда положить документ: ')
         if num_shelf not in self.direct_add.keys():
             return 'Такой полки не существует!'
         new_data = {}
-        for Ddata in ('type', 'number', 'name'):
-            new_data[Ddata] = get_input(f'Введите данные: {Ddata}')
+        # for Ddata in ('type', 'number', 'name'):
+            # new_data[Ddata] = input(f'Введите данные: {Ddata}')
         self.docs_add.append(new_data)
         self.direct_add[num_shelf].append(new_data['number'])
         for doc in self.docs_add:
-            print(doc['type'], doc['number'], doc['name'])
+            return f"{doc['type'], doc['number'], doc['name']}"
         return self.direct_add
     
 class work_buh():
     '''рабочий метод, в котором вводится команда'''
+    
+    def get_input(self, text):
+        return input(text)
+    
     documents = buh_data.documents
     directories = buh_data.directories
-    
+        
     get_args_to_class = Buhgalteria(
     documents,
     directories,
@@ -60,17 +65,24 @@ class work_buh():
     documents,
     directories
     )
-
-    while True:
-        print('Возможные команды: p, s, l, a')
-        comand = get_input('Введите название команды ')
-        if comand == 'p':
-            print(Buhgalteria.get_people_name(get_args_to_class))
-        elif comand == 's':
-            print(Buhgalteria.get_shelf_number(get_args_to_class))
-        elif comand == 'l':
-            print(Buhgalteria.get_list(get_args_to_class))
-        elif comand == 'a':
-            print(Buhgalteria.add_doc(get_args_to_class))
-        break
+    def huef(self):
+        while True:
+            print('Возможные команды: p, s, l, a')
+            comand = input('Введите название команды ')
+            if comand == 'p':
+                comand_p = Buhgalteria.get_people_name(self.get_args_to_class, input('Введите номер документа: '))
+                return comand_p
+            elif comand == 's':
+                comand_s = Buhgalteria.get_shelf_number(self.get_args_to_class, input('Введите номер документа: '))
+                return comand_s
+            elif comand == 'l':
+                comand_l = Buhgalteria.get_list(self.get_args_to_class)
+                return comand_l
+            elif comand == 'a':
+                data_lst = []
+                for Ddata in ('type ', 'number ', 'name '):
+                    Ddata = input(f'Введите данные: {Ddata}')
+                comand_a = Buhgalteria.add_doc(self.get_args_to_class, input('Введите номер полки, куда положить документ: '), Ddata)
+                return comand_a
+            # break
     
--- Home_work/DZ_20_test/tests/test_unittest.py
+++ Home_work/DZ_20_test/tests/test_unittest.py
@@ -1,24 +1,36 @@
-from unittest.mock import patch
 import unittest
+from unittest.mock import patch
+import buhgalteria_class
 
-def get_input(text):
-    return input(text)
-def answer():
-    ans = get_input('enter yes or no')
-    if ans == 'yes':
-        return 'you entered yes'
-    if ans == 'no':
-        return 'you entered no'
-class Test(unittest.TestCase):
-    # get_input will return 'yes' during this test
-    @patch('yourmodule.get_input', return_value='yes')
-    def test_answer_yes(self, input):
-        self.assertEqual(answer(), 'you entered yes')
-    @patch('yourmodule.get_input', return_value='no')
-    def test_answer_no(self, input):
-        self.assertEqual(answer(), 'you entered no')
+class TestBuhgalteria(unittest.TestCase):
+    
+    def setUp(self):
+        self.func = buhgalteria_class.Buhgalteria()
+        
+    # @patch('buhgalteria_class.Buhgalteria')
+    def test_comand_p(self):
+        self.assertEqual(self.func.get_people_name('11-2'), 'Иерофан Покемонов')
+        self.assertEqual(self.func.get_people_name('2207 876234'), 'Василий Питонов')
+        self.assertEqual(self.func.get_people_name('10006'), 'Эдуард Шевелюров')
+        
+    def test_comand_s(self):
+        self.assertEqual(self.func.get_shelf_number('11-2'), 'Документ находится на полке № 1')
+        self.assertEqual(self.func.get_shelf_number('2207 876234'), 'Документ находится на полке № 1')
+        self.assertEqual(self.func.get_shelf_number('10006'), 'Документ находится на полке № 2')
+        
+    def test_comand_l(self):
+        self.assertEqual(self.func.get_list(), [
+            'passport 2207 876234 Василий Питонов',
+            'invoice 11-2 Иерофан Покемонов',
+            'insurance 10006 Эдуард Шевелюров'
+            ])
+        
+        
+            
+    
+    # def test_Buh(self):
+    #     self.assertEqual(self.func.huef(), 'Иерофан Покемонов')
+
 
 if __name__ == '__main__':
     unittest.main()
-# if __name__ == '__main__':
-#     unittest.main()
