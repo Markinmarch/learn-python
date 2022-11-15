0a7a77af4513e5c5227309ca1ac5c8029223052c
Mark Izotov <iztvmrk@mail.ru>
2022-11-15 20:26:01 +0300

    Articles

--- Home_work/DZ_22_Django_1/listing.py
+++ Home_work/DZ_22_Django_1/listing.py
@@ -1,3 +1,5 @@
-# django-admin startproject Maun   начало работы с Django
-# python -m venv my_project создание виртуального окружения
-# python manage.py startapp products создание приложения
+# django-admin startproject Maun ===  начало работы с Django
+# python -m venv my_project === создание виртуального окружения
+# python manage.py startapp products === создание приложения
+# pip install -r 'путь к requrement.txt' === для установки пакетов
+# python manage.py runserver === запускает локальный тетсировочный сервер
