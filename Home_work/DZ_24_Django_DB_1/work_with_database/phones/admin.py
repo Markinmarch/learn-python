1e894cb8ad88efbe6a07e94404aeea9a89cc5d61
Mark Izotov <iztvmrk@mail.ru>
2022-11-24 21:24:37 +0300

    main

--- Home_work/DZ_24_Django_DB_1/work_with_database/phones/admin.py
+++ Home_work/DZ_24_Django_DB_1/work_with_database/phones/admin.py
@@ -5,5 +5,3 @@ from phones import models
 @admin.register(models.Phone)
 class PhoneAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'price','image','release_date','lte_exists','slug')
-
-admin.site.register(models.Phone, PhoneAdmin)
