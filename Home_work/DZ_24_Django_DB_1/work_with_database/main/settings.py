7f3e6345876a8dad19afaffa235629d9526894e5
Mark Izotov <iztvmrk@mail.ru>
2022-11-24 22:32:01 +0300

    main

--- Home_work/DZ_24_Django_DB_1/work_with_database/main/settings.py
+++ Home_work/DZ_24_Django_DB_1/work_with_database/main/settings.py
@@ -51,11 +51,11 @@ MIDDLEWARE = [
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'DZ',
+        'NAME': '',
         'HOST': '127.0.0.1',
         'PORT': '5432',
         'USER': 'postgres',
-        'PASSWORD': 'M4rk130t08',
+        'PASSWORD': '',
     }
 }
 
